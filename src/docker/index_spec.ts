168a405896238bbb19a9d2a3ade6674f0ee4f535
Emmanuel Demey <demey.emmanuel@gmail.com>
2018-09-05 10:44:29 +0200

    fix: solve unit test

--- src/docker/index_spec.ts
+++ src/docker/index_spec.ts
@@ -1,16 +1,14 @@
-import { Tree } from '@angular-devkit/schematics';
-import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
-import * as path from 'path';
+import { Tree } from "@angular-devkit/schematics";
+import { SchematicTestRunner } from "@angular-devkit/schematics/testing";
+import * as path from "path";
 
+const collectionPath = path.join(__dirname, "../collection.json");
 
-const collectionPath = path.join(__dirname, '../collection.json');
+describe("docker", () => {
+  it("works", () => {
+    const runner = new SchematicTestRunner("schematics", collectionPath);
+    const tree = runner.runSchematic("docker", {}, Tree.empty());
 
-
-describe('docker', () => {
-  it('works', () => {
-    const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = runner.runSchematic('docker', {}, Tree.empty());
-
-    expect(tree.files).toEqual(['/Dockerfile']);
+    expect(tree.files).toEqual(["/Dockerfile", "/config/nginx.conf"]);
   });
 });
